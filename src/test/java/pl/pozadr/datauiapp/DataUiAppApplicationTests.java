1c868a72639d2b9400fb3f0519d27abc01c31846
Pozadr <adpozor@gmail.com>
2020-12-10 14:54:19 +0100

    init

---
+++ src/test/java/pl/pozadr/datauiapp/DataUiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pozadr.datauiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataUiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
