79978f9ca2d06ea24e8629fe4f3b488ee47f7244
Pozadr <adpozor@gmail.com>
2020-12-12 14:25:00 +0100

    delete

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -8,8 +8,16 @@ $(document).ready(function () {
             $('#modelUpdate').val(sail.model);
             $('#markUpdate').val(sail.mark);
             $('#updatedByUpdate').val(sail.updatedBy);
-            $('#updatedOnUpdate').val(sail.updatedOn);
+            $('#updatedOnUpdate').val(sail.updatedOn).substr(0,10);
         });
         $('#editModal').modal();
     });
+
+    $('.table .deleteButton').on('click', function (event) {
+        event.preventDefault();
+        var href = $(this).attr('href');
+        $('#deleteModal #idDelete').attr('href', href);
+        $('#deleteModal').modal();
+    });
+
 });
